03e0592a65c020ce8aa069de2c39a772e394d36b
Raphaël Chalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-05-12 00:22:48 +1000

    Change title window

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 var myClock = setInterval(clock, 500);
+var settingTitle = setInterval(setTitle, 1000);
 
 function clock() {
     var date = new Date();
+    // setTitle(date);
     var time_us = date.toLocaleTimeString('en-US');
     var time_us_array = time_us.split(' ');
     $('#current_time').text(time_us_array[0]);
@@ -15,6 +17,31 @@ function clock() {
             $('#pm').text("PM");
             break;
     }
+}
+
+function setTitle(date) {
+    var date = new Date();
+    var time_us = date.toLocaleTimeString('en-US');
+    var time_us_array = time_us.split(' ');
+    var time_h_m_s = time_us_array[0].split(':');
+    var clock_emojis = [`🕐`, `🕜`, `🕐`, `🕝`, `🕒`, `🕞`, `🕓`, `🕟`, `🕔`, `🕠`, `🕕`, `🕡`, `🕖`, `🕢`, `🕗`, `🕣`, `🕘`, `🕤`, `🕙`, `🕥`, `🕚`, `🕦`];
+    var h = date.getHours();
+    if (h > 11) {
+        h -= 12;
+    }
+    var m = date.getMinutes();
+    var index = h*2;
+    if (m > 29) {
+        index-=1;
+    } else {
+        index -= 2;
+    }
+    if (index < 0) {
+        index += 2;
+    }
+    var emoji = clock_emojis[index]
+    $('title').text(emoji + ' ' + time_h_m_s[0] + ':' + time_h_m_s[1] + ' ' + time_us_array[1]);
+    return index;
 }
 
 var locale = Intl.DateTimeFormat().resolvedOptions().locale;
